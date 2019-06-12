46968c95293e4118ab2d30b186a2b40b2bf0b0c8
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-12 16:46:04 -0700

    working on app.js file

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,27 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import BarSearch from './components/SearchBar/searchbar';
+import PostCont from './components/PostContainer/postcontainer';
+import '../src/components/login.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mydata: dummyData,
+      local_user: "",
+      post_comment: "",
+    }
+  }
+
+// Function to save the state of all the likes
+
+triggerlikes = (e, username) => {
+  e.preventDefault();
+    this.setState(preState => {
+      const updatedlike = preState.mydata.map(mydataupdated => {
+        if (mydataupdated.username === username) {
+          mydataupdated.liked = !mydataupdated.liked
+          mydataupdated.liked ? mydataupdated.likes = mydataupdated.likes + 1 : mydataupdated.likes = mydataupdated.likes - 1
+        }
+        return mydataupdated;
+      })
+      return {
+        mydata : updatedlike
+      }
+    })
+  }
+
+  //Function to trigger user form if there is no local username state
+  postaction = (e, username) => {
+    e.preventDefault();
+    if(this.state.local_user === "") {
+      this.openForm()
+    } else {
+      this.savepost(e, username)
+    }
+  }
+
+  //Function to open the form
+  openForm() {
+    document.getElementById("myForm").style.display = "block"
+  }
+
+  //Function to close the form
+  closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  //Function to save the state of the username in the form
+  textchange = (e) => {
+    this.setState({local_user: e.target.value})
+    console.log(e.target.value)
+  }
+
+  //this is to close the form only.  state is already saved
+  Submit = (e) => {
+    e.preventDefault()
+    this.closeForm()
+    console.log(this.state.local_user)
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+
+
 }
 
 export default App;
