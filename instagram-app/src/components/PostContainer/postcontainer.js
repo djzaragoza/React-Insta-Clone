5e744f6a683acc846f2bf52cfde623b83fa704a1
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-13 22:16:27 -0700

    working on postcontainer.js file

---
+++ instagram-app/src/components/PostContainer/postcontainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './postcontainer.css';
+import Userinfo from './user/userinfo';
+import Postcontent from './postcontent/postcontent';
+import Useraction from './useractions/useraction';
+import Messagesection from './messagesection/messagesection';
+import Likes from './likes/likes';
+import Post from './post/post';
+
+const PostCont = (props) => {
+    return (
+        <div className="wrapper">
+            <div className="post_container">
+                
+            </div>
+        </div>
+    )
+}
