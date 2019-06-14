276324db96b56caa384ca1c1c00571a1419d4f3b
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-13 23:15:25 -0700

    finished working on postcontainer.js

--- instagram-app/src/components/PostContainer/postcontainer.js
+++ instagram-app/src/components/PostContainer/postcontainer.js
@@ -11,8 +11,17 @@ const PostCont = (props) => {
     return (
         <div className="wrapper">
             <div className="post_container">
-                
+                <Userinfo myuser =  {props.mydata}/>
+                <Postcontent mydata = {props.mydata} triggerlikes={props.triggerlikes}/>
+                <Useraction triggerlikes={props.triggerlikes} mydata={props.mydata}/>
+                <Likes likes_var = {props.mydata}/>
+                    {props.mydata.comments.map({comments_map, j} => (
+                        <Messagesection message = {comments_map} key={comments_map.com_id} />
+                    ))}
+                <Post postaction={props.postaction} postchange={props.postchange} mydata={props.mydata} post_comment={props.post_comment}/>
             </div>
         </div>
     )
 }
+
+export default PostCont;
