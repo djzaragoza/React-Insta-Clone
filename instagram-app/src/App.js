abbd3290979fbc8279fc5705ed95b7da4f8f97c9
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-14 22:11:58 -0700

    still working on app.js file

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -51,5 +51,56 @@ class App extends React.Component {
   closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
-  
+  //function to save the state of the user name in the form
+  textchange = (e) => {
+    this.setState({local_user: e.target.value})
+    console.log(e.target.value)
+  }
+  //this is to close the form only.  state is already saved
+  submit = (e) => {
+    e.preventDefault()
+    this.closeForm()
+    console.log(this.state.local_user)
+  }
+  //function to save the state of the post message
+  postchange = (e) => {
+    this.setState({
+      post_comment: e.target.value
+    })
+  }
+  //this is to save the post comment and username
+  savepost = (e, username) => {
+    e.preventDefault()
+    const currentuser = this.state.local_user;
+    const comment = this.state.post_comment;
+    const mydate = Date.now();
+      this.setState(prevState => {
+        const myupdatedpost = prevState.mydata.map(updatepost => {
+          if(updatepost.username === username) {
+            updatepost.comments.push({'com_id':mydate, 'username':currentuser , 'text':comment})
+            console.log(mydate, currentuser , comment)
+          }
+          this.setState({
+            post_comment: ""
+          })
+          return updatepost
+        })
+        return {
+          mydata: myupdatedpost
+        }
+      })
+  }
+
+  render() {
+    return(
+      <div className="app_css">
+      <div className="form-popup" id="myForm">
+        <form onSubmit={(e) =>this.submit(e)} className="form-container">
+          <h2>Enter User Name first</h2>
+          <h3>Click on "Post" after submit</h3>
+        </form>
+      </div>
+      </div>
+    )
+  }
 }
